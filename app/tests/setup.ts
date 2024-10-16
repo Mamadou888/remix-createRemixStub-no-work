4806f7dfccf5b34ac0d34a826996763d2fdfd231
Sam Chami <7530322+SamChami@users.noreply.github.com>
2024-10-16 13:30:26 -0700

    Removed more unused

--- app/tests/setup.ts
+++ app/tests/setup.ts
@@ -1,11 +1,4 @@
 import { installGlobals } from '@remix-run/node'
 import '@testing-library/jest-dom'
-import { afterAll, afterEach } from 'vitest'
 
 installGlobals()
-
-// one time setup here
-// fs.copyFileSync(BASE_DATABASE_PATH, DATABASE_PATH)
-
-afterEach(() => {})
-afterAll(async () => {})
